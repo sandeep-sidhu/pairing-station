ec8bb76b50bae5da82939747fcc71dc4aabfc7be
Pairing Station <chris@chrismaddern.com>
2015-02-15 10:30:56 -0500

    Show overlay when unlocking

--- Pair/AppDelegate.m
+++ Pair/AppDelegate.m
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 #import "PRGStationCoordinator.h"
 #import "PRGUser.h"
+@import AppKit;
 
 @interface AppDelegate ()
 
@@ -14,6 +15,20 @@
     self.stationCoordinator = [[PRGStationCoordinator alloc] init];
     [self.stationCoordinator initializePairingView];
     
+    [self.stationCoordinator showPairingOverlay];
+    [self configureToShowOnUnlock];
+}
+
+
+- (void)configureToShowOnUnlock {
+    [[NSDistributedNotificationCenter defaultCenter] addObserver:self
+                                                        selector:@selector(screenUnlocked:)
+                                                            name:@"com.apple.sessionAgent.SessionSwitchReady"
+                                                          object:nil];
+}
+
+
+- (void)screenUnlocked:(NSNotification *)notif {
     [self.stationCoordinator showPairingOverlay];
 }
 
